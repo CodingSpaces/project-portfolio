be387869e75c9e3daad63d459c1770f3f92af1b8
Isaac Christoffersen <ichristoffersen@vizuri.com>
2011-09-05 15:00:42 -0400

    Init

---
+++ project-portfolio.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package Project Portfolio
+ * @version 0.10
+ */
+/*
+Plugin Name: Project Portfolio
+Plugin URI:  https://github.com/CodingSpaces/project-portfolio
+Description: Plugin for a Project Portfolio.
+Author: ichristoffersen
+Version: 0.10
+Author URI: http://www.codingspaces.com/
+License: GPLv2 or later
+*/
+
+/*  Copyright 2011  Coding Spaces  ( isaac@codingspaces.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Project Portfolio ===
+Contributors: ichristoffersen
+Tags: project, portfolio, images gallery, custom fields
+Requires at least: 3.1
+Tested up to: 3.1
+Stable tag: 0.10
+
+Project Portfolio allows you to create pages for projects of interest.  These can either be your project portfolio or outside projects of interest.
+
+== Description ==
+
+Project Portfolio allows to create a unique page for displaying project information to include:
+  - description 
+  - project abstract 
+  - project URL
+  - source code url
+  - organization
+  - logo or screenshot
+
+== Installation ==
+
+1. Upload `project-portfolio` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+== Upgrade Notice ==
+
+
